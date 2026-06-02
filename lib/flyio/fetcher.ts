4f53aa766bb3f3bd4294d390ed8b63dd2d0a25be
Henry Gao <henrygao48@gmail.com>
2026-06-03 10:07:16 +1200

    Remove init optional headers for flyFetch
    Current goal is to make tech dashboard which I am
    assuming to be read-only thus shouldn't need request
    overrides

--- lib/flyio/fetcher.ts
+++ lib/flyio/fetcher.ts
@@ -1,19 +1,13 @@
 import { flyTokens } from "./config";
 
-export async function flyFetch(
-  url: string,
-  orgSlug: string,
-  init?: RequestInit
-): Promise<Response> {
+export async function flyFetch(url: string, orgSlug: string): Promise<Response> {
   const token = flyTokens.get(orgSlug);
   if (!token) throw new Error(`No token for org: ${orgSlug}`);
   
   return fetch(url, {
-    ...init,
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
-      ...(init?.headers ?? {}),
     },
   });
 }
